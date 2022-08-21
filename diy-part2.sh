2c46311f797463bd7797450621bddce78c9faf30
Ze-Zheng Wu <zzwu@zju.edu.cn>
2022-08-21 21:44:37 +0800

    Update diy-part2.sh
    add `--depth 1` to reduce git pull size

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git --depth 1 package/luci-app-openclash
